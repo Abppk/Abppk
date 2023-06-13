a6fa8f9487bf36513ee87fd8850b87e943208328
ABOOK <sky_816816@qq.com>
2023-06-13 11:28:37 +0800

    firstone

---
+++ src/main/java/com/testone/testone.java
@@ -0,0 +1,9 @@
+package com.testone;
+
+public class testone {
+    public static void main(String[] args) {
+        System.out.println("goor");
+        System.out.println("goor");
+        System.out.println("goor");
+    }
+}
